bff547131b777ecb5d10a9dfc62ce7606fb3f07a
Kenichi Ooi <kenoi@jp.ibm.com>
2022-02-01 17:26:58 +0900

    Add jaxrs war and related codes to the application

--- jaxrs-docker/jaxrs-start.sh
+++ jaxrs-docker/jaxrs-start.sh
@@ -2,4 +2,4 @@
 if [ ! -d ~/pdprof ]; then
     mkdir ~/pdprof
 fi
-docker run -d --name sib -p 9080:9080 -p 9443:9443 -v ~/pdprof:/pdprof jaxrs
+docker run -d --name jaxrs -p 9080:9080 -p 9443:9443 -v ~/pdprof:/pdprof jaxrs
